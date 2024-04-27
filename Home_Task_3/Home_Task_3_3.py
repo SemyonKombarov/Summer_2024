0b3ea6e1a930218e1bf481370d751afb36a3f428
KSV <semyonkombarov@yandex.ru>
2024-04-27 22:11:56 +0300

    fizzBuzz

---
+++ Home_Task_3/Home_Task_3_3.py
@@ -0,0 +1,24 @@
+n = input("Введите предложение ")
+#n = 'Я люблю Питон и кофе '
+x = n.split()
+#print(x)
+max1 = 0
+max2 = 0
+maximum = []
+lst = []
+#print(len(max(x)))
+for k, j in enumerate(x):
+    if len(j) > max1:
+        max1 = len(j)
+        maxi = j
+        max2 = k
+        #print(maxi)
+    lst.append(len(j))
+    if len(maxi) == len(j):
+        maximum.append(j)
+
+print(f"Первое наибольшее слово в предложении -{n}- это '", maxi, "'")
+print(f'Список со всеми самыми длинными словами по порядку {maximum}')
+
+        #print("max", max1, max2)
+
