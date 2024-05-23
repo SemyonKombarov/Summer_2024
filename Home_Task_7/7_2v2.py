04793a2c5020188b6e571705b5ada450997fd693
KSV <semyonkombarov@yandex.ru>
2024-05-23 11:34:20 +0300

    Home_Task_7_2v2-completed

--- Home_Task_7/7_2v2.py
+++ Home_Task_7/7_2v2.py
@@ -5,18 +5,23 @@ def code(n,m):
     dct = {}
     dct_d = {}
     for i in range(len(lst_abc)):
-        dct[lst_abc[i]] = dct.get(lst_abc[i], i + 1)
-        dct_d[i + 1] = dct_d.get((i + 1), lst_abc[i])
-
+        dct[lst_abc[i]] = dct.get(lst_abc[i], i)
+        dct_d[i] = dct_d.get((i), lst_abc[i])
+    # print(dct)
+    # print(dct_d)
     for i in n:
         if i in tes_abc:
-            if dct[i] + m >= 27:
-                print(dct_d[dct[i] + m - 26], end="")
+            if dct[i] + m <= 25:
+                print(dct_d[dct[i]+m], end="")
+
             else:
-                print(dct_d[dct[i] + m], end="")
-                continue
+                print((dct_d[(dct[i]+m)%26]), end="")
+
         else:
             print(i, end="")
+    return ""
+# n = "abcdefghijklmnopqrstuvwxyz z abcyxz"
+# m = 1
 
 n = str(input("Введите строку "))
 m = int(input("Введите ключ шифрования "))
