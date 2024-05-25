b3aa829b765d96fbe657ccdbe260d7b908f6fbbb
KSV <semyonkombarov@yandex.ru>
2024-05-25 13:52:51 +0300

    Home_Task_9

--- Home_Task_9/Home_Task_9_1.py
+++ Home_Task_9/Home_Task_9_1.py
@@ -12,6 +12,21 @@ for i in n:
     elif i == "A":
         lst.append("A")
 print("Решение через цикл","".join(lst))
+def zamena(i):
+    if i == "G":
+        j = "C"
+    elif i == "C":
+        j = "G"
+    elif i == "T":
+        j = "A"
+    elif i == "A":
+        j = "A"
+    return j
+
+print("Решение через функцию", end=" ")
+for i in n:
+    print(zamena(i), end="")
+
 
 
 
