9c1caa9199dc11e3adb3f599319c790f1a78b8fe
KSV <semyonkombarov@yandex.ru>
2024-07-09 22:45:51 +0300

    Home_Task_27

--- Home_Task_27/27_2.py
+++ Home_Task_27/27_2.py
@@ -6,13 +6,17 @@ class Item:
     def total(self):
         return self.price * self.quantity
 
-    def __getattr__(self, attr):
-        x = object.__getattr__(self, attr)
-        return x.title()
+    def __getattribute__(self, attr):
+        if attr == "name":
+            x = object.__getattribute__(self, attr)
+            return x.title()
+        else:
+            return object.__getattribute__(self, attr)
 
 book = Item("book1",100,10)
 cucumber = Item('cucumber',10,50)
 print(book.name)
+print(cucumber.name)
 print(book.total())
 print(cucumber.total())
 
